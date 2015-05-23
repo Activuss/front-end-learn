622d70ad153c767b8bbf02dec14fdc0972124fa9
Activuss <activuss@gmail.com>
2015-05-23 15:45:12 +0300

    initial

---
+++ Snake/scripts/helper/snakeGame.js
@@ -0,0 +1,12 @@
+/**
+ * Created by illya.kolykhanin on 23.05.2015.
+ */
+function SnakeJS() {
+    var defaultConfig = {
+        gridHeight : 20,
+        gridWidth : 20,
+        pointSize : 10
+    };
+
+
+}''
