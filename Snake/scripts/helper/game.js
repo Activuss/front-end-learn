0518924c1658cdf638eadb88af0d35ef00929026
Activuss <activuss@gmail.com>
2015-06-02 23:05:26 +0300

    added move logic

--- Snake/scripts/helper/game.js
+++ Snake/scripts/helper/game.js
@@ -1,11 +1,18 @@
 (function snake(){
 
     var config = {
-        boardHeight : 20,
-        boardWidth : 20,
-        snakeDefaultSize :3,
-        spawnCoordinateX : 0,
-        spawnCoordinateY : 0
+        BOARD_HEIGHT : 20,
+        BOARD_WIDTH : 20,
+        SNAKE_DEFAULT_SIZE :3,
+        SPAWN_COORDINATE_X : 1,
+        SPAWN_COORDINATE_Y : 1
+    }
+
+    var direction = {
+        UP : "up",
+        DOWN : "down",
+        LEFT : "left",
+        RIGHT : "right"
     }
 
     function SnakePart(options) {
@@ -18,30 +25,62 @@
     function Snake () {
         this.parts = [];
         this.length = 0;
-        var appendSnakePart = function (x, y) {
-            parts.push (new SnakePart(
+        var that = this;
+        this.appendSnakePart = function (x, y, direction) {
+            that.parts.push (new SnakePart(
                 {
                     x: x,
-                    y: y
+                    y: y,
+                    direction: direction
                 }
             ))
-            this.length++;
+            that.length++;
+            that.head = this.parts[this.length - 1];
         }
-        var snakSpawnSize = config.snakeDefaultSize;
-        var spawnX = config.spawnCoordinateX;
-        var spawnY = config.spawnCoordinateY;
-        while (snakSpawnSize-- > 0) {
-            /*this.parts.push(new SnakePart(
-                {   x : spawnX,
-                    y : spawnY++
-                    }))*/
-            appendSnakePart(spawnX, spawnY++);
+
+        var snakeSpawnSize = config.SNAKE_DEFAULT_SIZE;
+        var spawnX = config.SPAWN_COORDINATE_X;
+        var spawnY = config.SPAWN_COORDINATE_Y;
+        while (snakeSpawnSize-- > 0) {
+            this.appendSnakePart(spawnX, spawnY++, direction.DOWN);
         }
-        this.head = this.parts[0]
+
+        this.move = function(options) {
+            var currentHeadX = that.head.x;
+            var currentHeadY = that.head.y;
+            var shiftToDirection = function (x, y, direction){
+                that.parts.shift();
+                that.length--;
+                that.appendSnakePart(x, y, direction);
+            };
+
+            switch (options.direction) {
+                case "up": {
+                    shiftToDirection(currentHeadX, currentHeadY - 1, direction.UP);
+                    break;
+                }
+                case "down": {
+                    shiftToDirection(currentHeadX, currentHeadY - 1, direction.DOWN);
+                    break;
+                }
+                case "left": {
+                    shiftToDirection(currentHeadX - 1, currentHeadY, direction.LEFT);
+                    break;
+                }
+                case "right": {
+                    shiftToDirection(currentHeadX + 1, currentHeadY, direction.RIGHT);
+                    break;
+                }
+            }
+        }
 
     }
 
     var snake = new Snake()
+    //console.log(snake)
+    //snake.move({direction: "right"})
+    //snake.move({direction: "left"})
+    //snake.move({direction: "up"})
     console.log(snake)
 
 
