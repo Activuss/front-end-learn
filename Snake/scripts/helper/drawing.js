c8092ec9a07a86348e804a0bf3fc9d98668ef47d
Activuss <activuss@gmail.com>
2015-06-19 08:11:50 +0300

    able to draw background

--- Snake/scripts/helper/drawing.js
+++ Snake/scripts/helper/drawing.js
@@ -3,9 +3,10 @@ var game = game || {};
     function Drawer() {
         Drawer.prototype.drawBackground = function (width, height) {
             paper.setup('myCanvas');
-            var rectangle = new Rectangle(new Point(0, 0), new Point(width, height));
-             var path = new Path.Rectangle(rectangle);
+            var rectangle = new paper.Rectangle(new paper.Point(0, 0), new paper.Point(width, height));
+             var path = new paper.Path.Rectangle(rectangle);
              path.fillColor = '#808080';
+            paper.view.draw();
             /*var path = new Path.Rectangle([0, 0], [10, 10]);
             path.strokeColor = 'black';*/
             //alert('other module used, coordinates ' + width + ' ' + height);
